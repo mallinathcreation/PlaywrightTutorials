3fd336822cb41d61f07f9c4884c0b284885af6b4
mallinathcreation <mallinath180@gmail.com>
2026-04-25 20:24:42 +0530

    Commiting my changes

---
+++ tests/ClientAppOtherway.spec copy.js
@@ -0,0 +1,48 @@
+const { test, expect } = require('@playwright/test');
+
+
+
+test('Client App Login Other Way', async ({ page }) => {
+
+    const email = "mbasu@gmail.com"
+    const products = page.locator(".card-body");
+    const productName = "ZARA COAT 3";
+    await page.goto('https://rahulshettyacademy.com/client/');
+    await page.getByPlaceholder("email@example.com").fill(email);
+    await page.getByPlaceholder("enter your passsword").type("Mbasu@123");
+    await page.getByRole("button",{name: 'Login'}).click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+    
+    await page.locator(".card-body").filter({hasText: productName})
+    .getByRole("button",{name: 'Add To Cart'}).click();
+
+    //Important thing as if we give cart only then it will give two items
+    //Add to Cart and Cat. So thats why we are using like this
+    await page.getByRole("listitem").getByRole("button",{name: 'Cart'}).click();
+    await page.locator("div li").first().waitFor();
+    await expect(page.getByText(productName)).toBeVisible();
+    await page.getByRole("button",{name: 'Checkout'}).click();
+    
+    await page.getByPlaceholder("Select Country").pressSequentially("ind");
+    await page.getByRole("button",{name:'India'}).nth(1).click();
+    await page.getByText("PLACE ORDER").click();
+    await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+    const orderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderID);
+    await page.locator("li [routerlink*='myorders']").click();
+    await page.locator(".table").waitFor();
+    const rows = await page.locator("tbody tr");
+
+    for(let i=0; i< await rows.count(); i++){
+        const rowOrderID = await rows.nth(i).locator("th").textContent();
+        if(orderID.includes(rowOrderID)){
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderID.includes(orderIdDetails)).toBeTruthy();
+
+});
--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -2,6 +2,19 @@ import {test, expect} from '@playwright/test';
 
 test("Playwright special locators",async({page})=>
 {
-    await page.locator("https://rahulshettyacademy.com/angularpractice/");
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
     await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
+    await page.getByPlaceholder("Password").fill("abc123");
+    //Important thing
+    await page.getByRole("button",{name :'Submit'}).click();
+    //Important thing
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    //Important thing
+    await page.getByRole("Link",{name:'Shop'}).click();
+    //Playwright scans the multiple elements and then filters out by text what we give.
+    //Important thing. As we scan to the exact name and there is only one button
+    //so no need to provide the name
+    await page.locator("app-card").filter({hasText:'Nokia Edge'}).getByRole("button").click();
 })
